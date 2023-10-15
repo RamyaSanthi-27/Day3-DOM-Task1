c96c7db1ff7b2da427beaa0967122fae5274e37d
RamyaShanthi-27 <ramyashanthi@gmail.com>
2023-10-16 01:34:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var val=document.getElementById("result");
+
+function add(v){
+val.value+=v;
+}
+
+
+function cls(){
+val.value="";
+}
+
+function exe(){
+val.value=eval(val.value);
+}
+
+
+function cancel(){
+val.value=val.value.substr(0,val.value.length-1);
+}
